b6d9c603b9962969621995e1a8cbe969cd3c1e15
Dominic Melius <dominic@elbstack.com>
2015-12-28 01:05:12 +0100

    Added resolve-url loader to resolve relative paths in scss files

--- webpack/prod.config.js
+++ webpack/prod.config.js
@@ -35,7 +35,7 @@ module.exports = {
       { test: /(\.js|\.jsx)$/, exclude: /node_modules/, loaders: [strip.loader('debug'), 'babel']},
       { test: /\.json$/, loader: 'json-loader' },
       { test: /\.less$/, loader: ExtractTextPlugin.extract('style', 'css?reducers&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!less?outputStyle=expanded&sourceMap=true&sourceMapContents=true') },
-      { test: /(\.scss|\.css)$/, loader: ExtractTextPlugin.extract('style', 'css?reducers&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!sass?outputStyle=expanded&sourceMap=true&sourceMapContents=true') },
+      { test: /(\.scss|\.css)$/, loader: ExtractTextPlugin.extract('style', 'css?reducers&importLoaders=2&sourceMap!autoprefixer?browsers=last 2 version!resolve-url!sass?outputStyle=expanded&sourceMap=true&sourceMapContents=true') },
       { test: /\.woff(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/font-woff" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: "url?limit=10000&mimetype=application/octet-stream" },
